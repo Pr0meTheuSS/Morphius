716b88fd14ddb5b8955001b7c71c3be114cc2d80
Pr0meTheuSS <yr.olimpiev@gmail.com>
2022-11-19 00:03:11 +0700

    Runable static prototype

--- AutoPeter/Prototype.cpp
+++ AutoPeter/Prototype.cpp
@@ -14,18 +14,30 @@ void DrawPoligon(const std::valarray<float>* v, size_t amount) {
     glEnd();
 }
 
+void SetAutomorph(const GraphRepresentation& graph1, const GraphRepresentation& graph2) {
+    assert(graph1.GetSize() == graph2.GetSize());
+    
+}
 
 void DrawCircle(const std::valarray<float>& center, float radius, unsigned int granularity = 32) {
     glBegin(GL_LINE_LOOP);
     glColor3f(1.0f, 0.0f, 0.0f);
     for(unsigned int step = 0; step < granularity; step++) {
-        float theta = 2.0f * M_PI * float(step) / float(granularity);  // get the current angle
+        float theta = 2.0f * M_PI * float(step) / float(granularity);
 
-        float dx = radius * cosf(theta);  // calculate the x component
-        float dy = radius * sinf(theta);  // calculate the y component
+        float dx = radius * cosf(theta);
+        float dy = radius * sinf(theta);
 
-        glVertex2f(center[0] + dx, center[1] + dy);//output vertex
+        glVertex2f(center[0] + dx, center[1] + dy);
     }
+    glEnd();
+}
+
+void DrawLine(const std::valarray<float>& begin, const std::valarray<float>& end) {
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(begin[0], begin[1]);
+    glVertex2f(end[0], end[1]);
     glEnd();
 }
 
@@ -33,30 +45,38 @@ void DrawGraph(const GraphRepresentation& graph) {
     for (size_t node = 0; node < graph.GetSize(); node++) {
         DrawCircle(graph.GetPosition(node), 0.03f);
     }
+    // Replace to: for (auto edge: graph.Edges_) {DrawLine(edge.begin, edge.end)};
     for (size_t node1 = 0; node1 < graph.GetSize(); node1++) {
         for (size_t node2 = 0; node2 < graph.GetSize(); node2++) {
             if (graph.GetEdge({node1, node2})) {
-                // Draw line from node1 to node2
-                // TODO: implement
+                DrawLine(graph.GetPosition(node1), graph.GetPosition(node2));
             }
         }
     }
 }
-    // Init edges list and nodes amount.
-    const std::pair<size_t, size_t> peterGraphEdges[] = { {0,1}, {0,5},{0,4}, {1,2}, {1,6}, {2,3}, {2,7}, {3,4}, {3,8}, {4,9}, {5,7}, {5,8}, {6,8}, {6,9}, {7,9} };
-    const size_t peterGraphNodesAmount = 10;
+
+// Init edges list and nodes amount.
+const std::pair<size_t, size_t> peterGraphEdges[] = { {0,1}, {0,5},{0,4}, {1,2}, {1,6}, {2,3}, {2,7}, {3,4}, {3,8}, {4,9}, {5,7}, {5,8}, {6,8}, {6,9}, {7,9} };
+const size_t peterGraphNodesAmount = 10;
 GraphRepresentation peterGraphRepr(10);
 
-
 void displayloop() {
-   // Init graph's nodes positions.
-    //  ...
+    // Init graph's nodes positions.
     float radius = 0.3;
-    for (size_t node = 0; node < peterGraphRepr.GetSize(); node++) {
-        float theta = 2.0f * M_PI * float(node) / float(peterGraphRepr.GetSize()) + M_PI / 2.0f;  // get the current angle
-        int radScale = node / (peterGraphRepr.GetSize() / 2);
-        float x = radius * (radScale + 1) * cosf(theta);  // calculate the x component
-        float y = radius * (radScale + 1) * sinf(theta);  // calculate the y component
+    // Init external circle
+    for (size_t node = 0; node < peterGraphRepr.GetSize() / 2; node++) { 
+        float theta = 2.0f * M_PI * float(node) / float(peterGraphRepr.GetSize() / 2) + M_PI / 2.0f;
+
+        float x = radius * cosf(theta);
+        float y = radius * sinf(theta);
+        peterGraphRepr.SetPosition(node, {x, y});
+    }
+    // Init internal circle
+    for (size_t node = peterGraphRepr.GetSize() / 2; node < peterGraphRepr.GetSize(); node++) {
+        float theta = 2.0f * M_PI * float(node) / float(peterGraphRepr.GetSize() / 2) + M_PI / 2.0f;
+
+        float x = radius / 2.0f * cosf(theta);
+        float y = radius / 2.0f * sinf(theta);
         peterGraphRepr.SetPosition(node, {x, y});
     }
 
@@ -64,13 +84,12 @@ void displayloop() {
     DrawGraph(peterGraphRepr);
     glFlush();
 }
+
 int main(int argc, char** argv) {
     // Init graph representation.
-    for (size_t node = 0; node < sizeof(peterGraphEdges)/sizeof(peterGraphEdges[0]); node++) {
+    for (size_t node = 0; node < sizeof(peterGraphEdges) / sizeof(peterGraphEdges[0]); node++) {
         peterGraphRepr.AddEdge(peterGraphEdges[node]);
     }
-    
- 
     // Graphics part init and run main loop.
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
